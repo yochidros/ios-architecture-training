ca472482ed02db24d86db355e833e859a0cc6b80
yochidros <mm9.movement.trb@gmail.com>
2020-02-26 14:30:23 +0900

    feat: todo mvp

---
+++ TODO-MVP/TODO-MVP/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  TODO-MVP
+//
+//  Created by yochidros on 2020/02/26.
+//  Copyright © 2020 yochidros. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+	var window: UIWindow?
+
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+		// Override point for customization after application launch.
+		return true
+	}
+
+}
+
