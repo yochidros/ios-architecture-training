d637c32e8d39264ac2bb6f939c69e9c395b7d56a
yochidros <mm9.movement.trb@gmail.com>
2020-02-26 12:39:13 +0900

    add: mvc project.

---
+++ TODO-MVC/TODO-MVC/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  TODO-MVC
+//
+//  Created by yochidros on 2020/02/26.
+//  Copyright © 2020 yochidros. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+		// Override point for customization after application launch.
+		return true
+	}
+
+}
+
---
+++ TODO-MVC/TODO-MVC/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TODO-MVC
+//
+//  Created by yochidros on 2020/02/26.
+//  Copyright © 2020 yochidros. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
